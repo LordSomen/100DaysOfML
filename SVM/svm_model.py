e47a8a5be33ed1ca187fd34e0fd2aa6521a80fe0
LordSomen <soumyajit637@gmail.com>
2018-07-14 23:33:00 +0530

    continuing with svm

--- SVM/svm_model.py
+++ SVM/svm_model.py
@@ -20,3 +20,29 @@ svm_clf
 X_scaled  = StandardScaler(X)
 svm_clf.fit(X,Y)
 svm_clf.predict([[5.5,1.7]])
+
+#%%
+from sklearn.datasets import make_moons
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
+
+X,Y = make_moons()
+print(X)
+print(Y)
+
+#%%
+polynomial_svm_clf = Pipeline((
+    ("poly_features",PolynomialFeatures()),
+    ('scaler',StandardScaler()),
+    ('linear_svc',LinearSVC(C=10,loss="hinge")),
+))
+
+polynomial_svm_clf.fit(X,Y)
+
+#%%
+from sklearn.svm import SVC
+poly_kernel_svm_clf = Pipeline((
+("scaler", StandardScaler()),
+("svm_clf", SVC(kernel="poly", degree=3, coef0=1, C=5))
+))
+poly_kernel_svm_clf.fit(X, Y)

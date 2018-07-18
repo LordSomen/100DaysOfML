7840457267f2ed38aac6b3fe7bfe276e03f887a6
LordSomen <soumyajit637@gmail.com>
2018-07-18 17:45:16 +0530

    started with decision trees

---
+++ Decision_Trees/decision_tree.py
@@ -0,0 +1,22 @@
+#%%
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+iris = load_iris()
+X = iris['data'][:,:2]
+Y = iris['target']
+
+tree_clf = DecisionTreeClassifier(max_depth=2)
+tree_clf.fit(X,Y)
+
+#%%
+from sklearn.tree import export_graphviz
+export_graphviz(
+tree_clf,
+out_file="Decision_Trees/iris_tree.dot",
+feature_names=iris['feature_names'][2:],
+class_names=iris['target_names'],
+rounded=True,
+filled=True
+)
+

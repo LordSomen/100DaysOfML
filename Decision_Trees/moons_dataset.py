73540e9242cf824f3b35af62deef4c9cdf7678d9
LordSomen <soumyajit637@gmail.com>
2018-07-21 12:20:43 +0530

    trained the decision tree and test result 88%

--- Decision_Trees/moons_dataset.py
+++ Decision_Trees/moons_dataset.py
@@ -25,24 +25,31 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 param_grid = [
-    {'max_depth':[2,5,10,20],
-    'max_leaf_nodes':[5,10,15,20,50],
-    'min_impurity_split':[0.2,0.3,0.4]}
+    {
+    'max_depth':list(range(2,20)),
+    'max_leaf_nodes':list(range(2,100)),
+     'min_samples_split':[2,4,8,10,20],
+    }
 ]
 decision_tree_cls = DecisionTreeClassifier()
-_search = GridSearchCV(decision_tree_cls, param_grid, cv=10,
+_search = GridSearchCV(decision_tree_cls, param_grid, cv=5,
 scoring='neg_mean_squared_error')
 _search.fit(X_train, Y_train)
 _search.best_params_
 
 #%%
-# from sklearn.model_selection import RandomizedSearchCV
-# _search = RandomizedSearchCV(decision_tree_cls, 
-#             param_distributions=param_grid,
-#             n_iter=50, cv=5, scoring='neg_mean_squared_error',
-#             verbose=2, n_jobs=4, random_state=42)
-# _search.fit(X_train, Y_train)
-# _search.best_params_
+from sklearn.model_selection import RandomizedSearchCV
+param_random = {
+    'max_depth':[2,5,8,10,20,25],
+    'max_leaf_nodes':[5,10,15,18,20,25,30,35,50],
+    'min_impurity_split':[0.2,0.3,0.4,0.5]
+}
+_search = RandomizedSearchCV(decision_tree_cls, 
+            param_distributions=param_random,
+            n_iter=50, cv=5, scoring='neg_mean_squared_error',
+            verbose=2, n_jobs=4, random_state=42)
+_search.fit(X_train, Y_train)
+_search.best_params_
 
 
 #%%
@@ -53,6 +60,10 @@ prediction = final_model.predict(X_test)
 mse = mean_squared_error(Y_test,prediction)
 rmse = np.sqrt(mse)
 rmse
-# scores_forest = cross_val_score(grid_search)
-#  cv=10)
 
+
+#%%
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(Y_test,prediction)
+accuracy #accuracy = 0.8735

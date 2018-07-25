b464b3e9780a961b23143be9a664125d21f60fff
LordSomen <soumyajit637@gmail.com>
2018-07-25 12:56:12 +0530

    Random Forest

--- Ensemble_learning/ensemble_learning.py
+++ Ensemble_learning/ensemble_learning.py
@@ -61,3 +61,27 @@ bag_clf = BaggingClassifier(DecisionTreeClassifier(),
 bag_clf.fit(X_train,Y_train)
 print(bag_clf.oob_score_)
 
+#%%
+'''Random Forest'''
+from sklearn.ensemble import RandomForestClassifier
+
+rnd_clf = RandomForestClassifier(n_estimators=500,max_leaf_nodes=16,
+            n_jobs=-1)
+rnd_clf.fit(X_train,Y_train)
+Y_pred = rnd_clf.predict(X_test)
+print(accuracy_score(Y_test,Y_pred))
+
+#%%
+'''Equivalent BaggingClassifier of the above randomForestCLassifier'''
+bag_clf = BaggingClassifier(
+DecisionTreeClassifier(splitter="random", max_leaf_nodes=16),
+n_estimators=500, max_samples=1.0, bootstrap=True, n_jobs=-1
+)
+
+#%%
+from sklearn.datasets import load_iris
+iris = load_iris()
+rnd_clf = RandomForestClassifier(n_estimators=500, n_jobs=-1)
+rnd_clf.fit(iris["data"], iris["target"])
+for name, score in zip(iris["feature_names"], rnd_clf.feature_importances_):
+    print(name, score)

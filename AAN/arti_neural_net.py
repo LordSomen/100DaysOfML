40566987edc4de1478e396fbd5c8beb2c4582d1f
LordSomen <soumyajit637@gmail.com>
2018-10-14 22:44:41 +0530

    constructed cost func and optimizer

--- AAN/arti_neural_net.py
+++ AAN/arti_neural_net.py
@@ -89,4 +89,40 @@ def neuron_layer(X,n_neurons,name,activation=None):
 
 #%%
 
+with tf.name_scope("dnn"):
+    hidden1 = neuron_layer(X , n_hidden1 , name="hidden1" ,
+     activation=tf.nn.relu)
+    hidden2 = neuron_layer(hidden1,n_hidden2,name="hidden2",
+    activation=tf.nn.relu)
+    logits = neuron_layer(hidden2,n_outputs,name="outputs")
+
+#%%
+
+with tf.name_scope("loss"):
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=
+    Y,logits=logits)
+    loss = tf.reduce_mean(xentropy,name="loss")
+
+
+#%%
+
+learning_rate = 0.01
+
+with tf.name_scope("train"):
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    training_op = optimizer.minimize(loss)
+
+#%%
+with tf.name_scope("eval"):
+    correct = tf.nn.in_top_k(logits,Y,1)
+    accuracy = tf.reduce_mean(tf.cast(correct,tf.float32))
+
+#%%
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+
+#%%
+
+
 
